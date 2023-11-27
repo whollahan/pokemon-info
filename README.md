834acf170245f7cd9e0e077968afd612f417fc89
whollahan <hollahan.william@gmail.com>
2023-11-27 10:34:07 -0600

    initialization

---
+++ README.md
@@ -0,0 +1,21 @@
+### Pokemon Pokedex
+This project is a simple Pokedex application built with React and Material-UI. It uses the PokeAPI to fetch data about different Pokemon.
+
+### Features
+Search for Pokemon: Enter the name of a Pokemon in the search box and hit submit to view information about it. The search is case-insensitive.
+View Pokemon Details: The application displays various details about the Pokemon, including its name, image, and moves.
+Browse Pokemon Images: The application fetches all available images of the Pokemon and allows you to browse through them.
+Favorite Pokemon: Click the star next to a Pokemon's name to add it to your favorites. Click the star again to remove it from your favorites.
+View Favorites: Click the hamburger menu at the top right of the screen to view your favorited Pokemon. Click a Pokemon in the list to view its details.
+### Installation
+Clone this repository: git clone https://github.com/yourusername/pokemon-pokedex.git
+Install the dependencies: npm install
+Start the application: npm start
+### Usage
+To use the application, simply enter the name of a Pokemon in the search box and hit submit. The application will display information about the Pokemon. Click the star next to the Pokemon's name to add it to your favorites.
+
+### Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+### License
+MIT
---
+++ src/App.js
@@ -0,0 +1,198 @@
+import logo from './newHutao8.png';
+import './App.css';
+import Pokedex from 'pokedex-promise-v2';
+import { Fragment, useEffect, useState } from 'react';
+import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, IconButton, Drawer, List, ListItem, ListItemText, Menu, TextField } from '@mui/material';
+import StarBorder from '@mui/icons-material/StarBorder';
+import Star from '@mui/icons-material/Star';
+import MenuIcon from '@mui/icons-material/Menu';
+
+function App() {
+  const [pokemon, setPokemon] = useState('')
+  const [chosenPokemon, setChosenPokemon] = useState(null)
+  const [pokeSprites, setPokeSprites] = useState({})
+  const [index, setIndex] = useState(0);
+  const [spritesArray, setSpritesArray] = useState([]);
+  const [showGames, setShowGames] = useState(false);
+  const [showMoves, setShowMoves] = useState(false);
+  const [currentGameIndex, setCurrentGameIndex] = useState(0);
+  const [currentMoveIndex, setCurrentMoveIndex] = useState(0);
+  const [favorites, setFavorites] = useState([]);
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isFavorited, setIsFavorited] = useState(false);
+  const [invalidPokemon, setInvalidPokemon] = useState(false);
+
+  useEffect(() => {
+    createSpritesArray()
+  }, [pokeSprites])
+
+  useEffect(() => {
+    setIsFavorited(favorites.some(pokemon => pokemon.name === chosenPokemon.name));
+    console.log('favs', favorites)
+  }, [favorites, chosenPokemon]);
+
+  useEffect(() => {
+    console.log('pic', spritesArray[index])
+  }, [index])
+
+  const flattenSprites = (sprites) => {
+    let result = [];
+    for (let key in sprites) {
+      if (typeof sprites[key] === 'string') {
+        result.push(sprites[key]);
+      } else if (typeof sprites[key] === 'object') {
+        result = result.concat(flattenSprites(sprites[key]));
+      }
+    }
+    return result;
+  };
+
+  const P = new Pokedex();
+  const getPokemonData = async (pokemon) => {
+    console.log('pokemon', pokemon)
+    setInvalidPokemon(false)
+    try {
+      P.getPokemonByName(pokemon)
+        .then((res) => {
+          console.log('pokemon info', res)
+          setChosenPokemon(res);
+          setSpritesArray(flattenSprites(res.sprites)); // Flatten sprites object into array
+        })
+        .catch((err) => {
+          console.log('pokemon doesnt exist');
+          setInvalidPokemon(true)
+          setChosenPokemon(null);
+        });
+    } catch (err) {
+      console.log('Err: ', err);
+    }
+  };
+
+  const createSpritesArray = async () => {
+    let sprites = Object.entries(pokeSprites).map(([k, v]) => ({ v }))
+    console.log('spritesaaa', sprites)
+    setSpritesArray(sprites)
+  }
+
+  const cycleSpritesNext = () => {
+    setIndex((index + 1) % spritesArray.length);
+  };
+
+  const cycleSpritesPrev = () => {
+    setIndex((index - 1 + spritesArray.length) % spritesArray.length);
+  };
+
+  const toggleFavorite = () => {
+    setIsFavorited(!isFavorited);
+    if (!isFavorited) {
+      setFavorites([...favorites, chosenPokemon]);
+    } else {
+      setFavorites(favorites.filter(pokemon => pokemon.name !== chosenPokemon.name));
+    }
+  };
+
+  const handlePokemonData = (pokemonName) => {
+    getPokemonData(pokemonName);
+    setPokemon('');
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MenuIcon onClick={() => setIsNavOpen(true)} style={{ position: 'absolute', top: 30, right: 30 }}>
+          <Menu />
+        </MenuIcon>
+
+        <Drawer className="sidebar" anchor="right" open={isNavOpen} onClose={() => setIsNavOpen(false)}>
+          <Typography variant="h5">Favs</Typography>
+          <List style={{ backgroundColor: '#ddd' }}>
+            {favorites.map((poke, index) => (
+              <ListItem button key={index} onClick={() => { getPokemonData(poke.name); setIsNavOpen(false); }}>
+                <ListItemText primary={poke.name} />
+                <img src={poke.sprites.front_default} alt="pokemon" />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="pokeStuff">
+          <h1>Search a Pokemon to see information about them.</h1>
+          <TextField
+            type="text"
+            value={pokemon}
+            onChange={(e) => setPokemon(e.target.value)}
+            variant="outlined"
+            style={{ marginBottom: '20px', backgroundColor: 'rgb(213 213 213)', marginRight: '20px' }}
+          /><br />
+          <Button
+            id="pokemonButton"
+            onClick={() => handlePokemonData(pokemon.toLowerCase())}
+            variant="contained"
+            style={{ backgroundColor: '#3f51b5', color: '#fff' }}
+          >
+            Submit
+          </Button>
+
+          {invalidPokemon ? <p style={{ color: 'red' }}>Please enter a valid pokemon.</p> : null}
+
+          {chosenPokemon ?
+            <div className="pokemonInformation">
+              <p className="pokemonName">
+                {chosenPokemon.name.charAt(0).toUpperCase()}{chosenPokemon.name.slice(1)}
+                <IconButton onClick={toggleFavorite}>
+                  {isFavorited ? <Star /> : <StarBorder />}
+                </IconButton>
+              </p>
+              <div className="pokemonSprite">
+                <button className="arrowButton" onClick={cycleSpritesPrev}> &#8592; </button>
+                <img className="pokePic" src={spritesArray[index]} alt="pokemon" />
+                <button className="arrowButton" onClick={cycleSpritesNext}> &#8594; </button>
+              </div>
+              <div className="pokemonStats">
+                <p className="pokeStats">Height: {chosenPokemon.height}</p>
+                <p className="pokeStats">Weight: {chosenPokemon.weight}</p>
+                <p className="pokeStats">Base Experience: {chosenPokemon.base_experience}</p>
+                <p className="pokeStats">Abilities: {chosenPokemon.abilities.map((ability) => ability.ability.name).join(', ')}</p>
+                <p className="pokeStats">Types: {chosenPokemon.types.map((type) => type.type.name).join(', ')}</p>
+              </div>
+              <div>
+                {showMoves ? <Button variant="contained" style={{ backgroundColor: '#3f51b5', color: '#fff' }} onClick={() => setCurrentMoveIndex(prevIndex => prevIndex > 0 ? prevIndex - 1 : chosenPokemon.moves.length - 1)}> &#8592; </Button> : null}
+                <Button variant="contained" style={{ backgroundColor: '#3f51b5', color: '#fff' }} onClick={() => setShowMoves(!showMoves)}>Moves</Button>
+                {showMoves ? <Button variant="contained" style={{ backgroundColor: '#3f51b5', color: '#fff' }} onClick={() => setCurrentMoveIndex(prevIndex => prevIndex < chosenPokemon.moves.length - 1 ? prevIndex + 1 : 0)}> &#8594; </Button> : null}
+              </div>
+              {showMoves && (
+                <>
+                  <Typography style={{ marginBottom: '40px', marginTop: '40px' }} variant="h4">{chosenPokemon.moves[currentMoveIndex].move.name.charAt(0).toUpperCase() + chosenPokemon.moves[currentMoveIndex].move.name.slice(1)}</Typography>
+                  <TableContainer component={Paper} style={{ backgroundColor: '#f5f5f5', marginBottom: '40px' }}>
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgb(0 0 0)', padding: '10px 0', width: '100%' }}>
+                      <Table className="pokemonMoves" style={{ width: '98%' }}>
+                        <TableHead>
+                          <TableRow style={{ backgroundColor: '#3f51b5' }}>
+                            <TableCell style={{ color: '#fff' }}>Game</TableCell>
+                            <TableCell style={{ color: '#fff' }}>Level Learned At</TableCell>
+                            <TableCell style={{ color: '#fff' }}>Move Learn Method</TableCell>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          {chosenPokemon.moves[currentMoveIndex].version_group_details.map((detail, index) => (
+                            <TableRow key={index} style={{ backgroundColor: index % 2 === 0 ? '#e0e0e0' : '#fff' }}>
+                              <TableCell>{detail.version_group.name}</TableCell>
+                              <TableCell>{detail.level_learned_at}</TableCell>
+                              <TableCell>{detail.move_learn_method.name.charAt(0).toUpperCase() + detail.move_learn_method.name.slice(1)}</TableCell>
+                            </TableRow>
+                          ))}
+                        </TableBody>
+                      </Table>
+                    </div>
+                  </TableContainer>
+                </>
+              )}
+            </div>
+            : null}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
